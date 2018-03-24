d1c5f58fadb6e485a14577cce40c6598f0a085a8
Colin Morgan <colin.morgan@fullbit.ca>
2018-03-23 21:47:00 -0400

    Setting up templates and layout

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, flash
 
 
 reminders = Blueprint('reminders', __name__, template_folder="templates")
@@ -6,4 +6,5 @@ reminders = Blueprint('reminders', __name__, template_folder="templates")
 
 @reminders.route('/')
 def hello():
-    return "Hello, world!"
+    flash("Hello, world!", 'success')
+    return render_template('index.html')
