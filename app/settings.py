1ea0b9c95e5f7f01cf03d0cf4dd62ac40dfc1fd5
Colin Morgan <colin.morgan@fullbit.ca>
2018-04-17 13:15:08 -0230

    Integrated Twilio api

--- app/settings.py
+++ app/settings.py
@@ -7,6 +7,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_RESULT_BACKEND = 'redis://localhost:6379'
     CELERY_BROKER_URL = 'redis://localhost:6379'
+    TWILIO_AUTH_ID = os.environ.get('TWILIO_AUTH_ID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+    TWILIO_TO_NUMBER = os.environ.get('TWILIO_TO_NUMBER')
+    TWILIO_FROM_NUMBER = os.environ.get('TWILIO_FROM_NUMBER')
 
 
 class ProdConfig(Config):
--- app/tasks.py
+++ app/tasks.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from flask import current_app
 from sqlalchemy import and_
+from twilio.rest import Client
 
 from .extensions import celery, db
 from .models import Reminder
@@ -9,13 +10,18 @@ from .models import Reminder
 
 @celery.task()
 def send_reminders():
+    client = Client(current_app.config['TWILIO_AUTH_ID'],
+                    current_app.config['TWILIO_AUTH_TOKEN'])
     now = datetime.now()
     reminders = Reminder.query.filter(
         and_(Reminder.send_on <= now,
              Reminder.sent_on == None)).all()
 
     for reminder in reminders:
-        # TODO - Send SMS Reminder message
+        client.messages.create(
+            from_=current_app.config['TWILIO_FROM_NUMBER'],
+            to=current_app.config['TWILIO_TO_NUMBER'],
+            body=reminder.message)
         reminder.sent_on = now
         db.session.add(reminder)
         db.session.commit()
